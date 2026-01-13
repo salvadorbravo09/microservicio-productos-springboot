8da8965e1033d68b09359724a1be402219499589
Salvador Bravo <sa.bravo0901@gmail.com>
2026-01-12 22:44:05 -0300

    feat: se agregaron los repositorios de Categoria y Producto

---
+++ src/main/java/com/service/ms/product/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.service.ms.product.repositories;
+
+import com.service.ms.product.models.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
---
+++ src/main/java/com/service/ms/product/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.service.ms.product.repositories;
+
+import com.service.ms.product.models.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
