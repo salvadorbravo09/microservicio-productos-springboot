427fce5092a9f18979f6c5cdefe2ddae5f4f1b34
Salvador Bravo <sa.bravo0901@gmail.com>
2026-01-12 22:54:26 -0300

    feat: agregar método para buscar productos por ID de categoría

--- src/main/java/com/service/ms/product/repositories/ProductRepository.java
+++ src/main/java/com/service/ms/product/repositories/ProductRepository.java
@@ -4,6 +4,14 @@ import com.service.ms.product.models.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    /**
+     * Category --> campo category en la entidad Product
+     * _Id --> la propiedad id de la entidad Category
+     * **/
+    List<Product> findByCategory_Id(Long categoryId);
 }
