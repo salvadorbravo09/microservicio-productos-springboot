d070cba8dc8f1aaf5984ee0c956288b96ff817e9
Salvador Bravo <sa.bravo0901@gmail.com>
2026-01-16 23:21:52 -0300

    feat: agregar DTO para la respuesta de categorías

---
+++ src/main/java/com/service/ms/product/models/dtos/CategoryResponseDto.java
@@ -0,0 +1,19 @@
+package com.service.ms.product.models.dtos;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CategoryResponseDto {
+
+    private Long id;
+    private String name;
+    private String description;
+}
